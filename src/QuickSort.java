dd44e75cbedfc87793a89062570cbc10da181379
shiva <shivateja219@gmail.com>
2017-08-06 12:54:54 +0530

    Added comments

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -10,14 +10,15 @@
  */
 public class QuickSort {
     
-    static int partition(int[] A, int p, int q){
-       // set the pivot as the last element
-       int pivot  = A[q];
+    static int partition(int[] A, int p, int r){
+       // set the pivot as the last element and store it in a new variable
+       int pivot  = A[r];
        // i is a pointer beyond which the elements greater than pivot are stored
-       int i = p-1;
-       int r = A.length;
+       int i = p - 1;
+       // j moves from p to r-1
        int j;
         for(j = p; j < r; j++){
+            // Check if A[j] is less than or equal to pivot else ignore and increment j
             if(A[j] <= pivot){
                 // increment i
                 i++;
@@ -27,13 +28,15 @@ public class QuickSort {
                 A[j] = A[i];
                 A[i] = temp;
             }
-        }
-        q = i+1;
+        } 
+       // i is a boundary between the elements less than or equal to pivot and greater than pivot 
+       int q = i+1;
        int temp;
+       // increment i and place the pivot at i+1(correct position) i.e., exchange A[i+1] and A[r]
        temp = A[i+1];
        A[i+1] = A[r];
        A[r] = temp;           
-       // j is pointer for iterating through the array
+       // return the index of the pivot after it is moved to correct position
        return q;
     }
     
@@ -47,9 +50,9 @@ public class QuickSort {
     
     public static void main(String...args){
         int []a = {9, 6, 5, 0, 8, 2, 7};
-        quickSort(a, 0, 6);
+        quickSort(a, 0, a.length - 1);
         for(int temp: a)
-        System.out.println(a);
+        System.out.print(temp + " ");
     }
     
 }
